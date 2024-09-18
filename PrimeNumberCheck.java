a2cfdafbc57dc4aa6f9d47eb2fa445ca91e4390e
mannatraheja9 <mannatraheja779@gmail.com>
2024-09-18 00:20:41 -0700

    Add files via upload

---
+++ PrimeNumberCheck.java
@@ -0,0 +1,22 @@
+public class PrimeNumberCheck {
+    public static void main(String[] args) {
+        int number = 17;
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
