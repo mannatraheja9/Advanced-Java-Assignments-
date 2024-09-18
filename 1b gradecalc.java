2be9962e44da49eb48e4d14288187ae0d57df8aa
mannatraheja9 <mannatraheja779@gmail.com>
2024-09-18 02:18:44 -0700

    Update and rename gradecalc.java to 1b gradecalc.java

--- 1b gradecalc.java
+++ 1b gradecalc.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class gradecalc {
+public class Gradecalc {
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your score: ");
